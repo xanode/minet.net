ecc3f90febf3e2f51ef3c98d68906c50f32857a7
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-08-18 17:34:49 +0200

    Add vercel integration to enable analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   integrations: [tailwind()],
   site: 'https://minet.xanode.fr',
+  output: "static",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
