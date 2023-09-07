6589752531770fcb9f934beac787f0e58454a91f
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-09-07 15:04:11 +0200

    Fix favicon display

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-  ],
+  integrations: [tailwind(), mdx()],
   site: "https://minet.xanode.fr",
   output: "hybrid",
   adapter: vercel({
-    analytics: true,
-  }),
+    analytics: true
+  })
 });
