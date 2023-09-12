3d76003be27efe9a67f0a982fed9b143a89d90d4
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-09-12 10:34:59 +0200

    Update readme and remove vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: "https://minet.xanode.fr",
-  output: "hybrid",
-  adapter: vercel({
-    analytics: true
-  })
 });
