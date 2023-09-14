5fe735ea55531047334d2380d40ca85aff69b20f
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-09-14 23:26:10 +0200

    Improve structure of `search.json` static endpoint for future logic imrpovements

--- src/pages/search.json.ts
+++ src/pages/search.json.ts
@@ -1,15 +1,24 @@
 import { getCollection } from "astro:content";
 import type { APIContext, APIRoute } from "astro";
+import type { CollectionEntry } from "astro:content";
+
+const getData = async (tutorial: CollectionEntry<"tutoriels">) => {
+    return {
+        title: tutorial.data.title,
+        slug: tutorial.slug,
+        body: tutorial.body,
+    };
+}
+
+const getTutorials = async () => {
+    const tutorials = await getCollection("tutoriels");
+    return Promise.all(tutorials.map(tutorial => getData(tutorial)));
+};
 
 export const GET: APIRoute = async (context: APIContext) => {
-    const tutorials = await getCollection("tutoriels");
-    return new Response(
-        JSON.stringify(tutorials.map(tutorial => {
-            return {
-                title: tutorial.data.title,
-                slug: tutorial.slug,
-                body: tutorial.body
-            };
-        })),
-    );
+    return new Response(JSON.stringify(await getTutorials()), {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
 }
