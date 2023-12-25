d5484ec200f689648b05dfe3d313b04ce0745e45
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2023-12-25 19:00:30 +0100

    Refactor debounce function to fix typescript error

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -9,14 +9,14 @@ export function debounce<T extends (...args: any[]) => void>(func: T, delay: num
     let timeoutId: number;
 
     // The returned function will be executed when the debounced function is called.
-    return function (...args: Parameters<T>): void {
+    return function (this: any, ...args: Parameters<T>): void {
         // Clear the previous timeout to prevent the function from executing
         clearTimeout(timeoutId);
 
         // Set a new timeout to delay the execution of the function
         timeoutId = setTimeout(() => {
             // Call the original function with the provided arguments
-            func.apply(this as any, args);
+            func.apply(this, args);
         }, delay);
     };
 }
